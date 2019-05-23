9f64ef1bf6ae4b583bb331c56d766d7a725ad777
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2019-05-23 10:44:37 +1000

    adding code snippet demonstrating how to download data from gdrive

---
+++ DownloadFromGoogleDrive/download_gdrive.py
@@ -0,0 +1,34 @@
+
+# taken from https://github.com/CellProfiler/CellProfiler-plugins/blob/master/classifypixelsunet.py
+# which in turn references:
+# https://stackoverflow.com/a/39225272
+
+import requests
+
+def download_file_from_google_drive(id, destination):
+    url = "https://docs.google.com/uc?export=download"
+    session = requests.Session()
+    response = session.get(url, params={'id': id}, stream=True)
+    token = get_confirm_token(response)
+    if token:
+        params = {
+            'id': id,
+            'confirm': token
+        }
+        response = session.get(url, params=params, stream=True)
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+
+def save_response_content(response, destination):
+    chunk_size = 32768
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(chunk_size):
+            if chunk:  # filter out keep-alive new chunks
+                 f.write(chunk)
