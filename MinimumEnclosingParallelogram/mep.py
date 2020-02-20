d876cd0e64bda32a88db22b654e2c7dc9fccc726
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2020-02-20 16:18:28 +1100

    combined mep with rotating calipers and napari for playing around

--- MinimumEnclosingParallelogram/mep.py
+++ MinimumEnclosingParallelogram/mep.py
@@ -1,12 +1,38 @@
-# This code sample is from stack overflow:
+# some experiments with convex hull, rotating calipers and minimum enclosing paralellograms in napari
+#
+# What to do:
+# run
+# add a polygon in the napari shape layer
+# press 9
+#
+# After pressing 9, the convex hull, the minimum enclosing parallelogram and the minimum and maximm caliper
+# diameter will be shown on top of the polygon
+# layer numbers are hard-coded as this was just a quick test
+# 
+
+
+# contains this code sample is from stack overflow:
 # https://stackoverflow.com/questions/38409156/minimal-enclosing-parallelogram-in-python
 # not tested so far, not sure whehter this is using rotating calipers
 
 # a Matlab implementation (more generic, supports other shapes) is here
 # https://au.mathworks.com/matlabcentral/fileexchange/34767-a-suite-of-minimal-bounding-objects
 
-import math
 
+# also added this code:
+# This code snippet is taken from 
+# http://code.activestate.com/recipes/117225-convex-hull-and-diameter-of-2d-point-sets/
+# convex hull (Graham scan by x-coordinate) and diameter of a set of points
+# David Eppstein, UC Irvine, 7 Mar 2002
+
+# According to that website the code is under the PSF licencse
+# https://en.wikipedia.org/wiki/Python_Software_Foundation_License
+
+
+import math
+import napari
+from scipy.spatial import ConvexHull
+import numpy as  np
 """
 Minimal Enclosing Parallelogram
 
@@ -106,3 +132,91 @@ def mep(convex_polygon):
             so, ao, bo, co, do, z1o, z2o = st, at, bt, ct, dt, z1, z2
 
     return so, ao, bo, co, do, z1o, z2o
+
+
+# volker's testing code:
+
+points = [(0,1), (2,2), (5,3), (4,1.5), (4.5,2), (5,-1), (3,0)]
+# probably have to build convex hull first
+print(mep(points))
+
+
+
+
+def orientation(p,q,r):
+    '''Return positive if p-q-r are clockwise, neg if ccw, zero if colinear.'''
+    return (q[1]-p[1])*(r[0]-p[0]) - (q[0]-p[0])*(r[1]-p[1])
+
+
+def hulls(Points):
+    '''Graham scan to find upper and lower convex hulls of a set of 2d points.'''
+    U = []
+    L = []
+    Points.sort()
+    for p in Points:
+        while len(U) > 1 and orientation(U[-2],U[-1],p) <= 0: U.pop()
+        while len(L) > 1 and orientation(L[-2],L[-1],p) >= 0: L.pop()
+        U.append(p)
+        L.append(p)
+    return U,L
+
+def rotatingCalipers(Points):
+    '''Given a list of 2d points, finds all ways of sandwiching the points
+between two parallel lines that touch one point each, and yields the sequence
+of pairs of points touched by each pair of lines.'''
+    U,L = hulls(Points)
+    i = 0
+    j = len(L) - 1
+    while i < len(U) - 1 or j > 0:
+        yield U[i],L[j]
+        
+        # if all the way through one side of hull, advance the other side
+        if i == len(U) - 1: j -= 1
+        elif j == 0: i += 1
+        
+        # still points left on both lists, compare slopes of next hull edges
+        # being careful to avoid divide-by-zero in slope calculation
+        elif (U[i+1][1]-U[i][1])*(L[j][0]-L[j-1][0]) > \
+                (L[j][1]-L[j-1][1])*(U[i+1][0]-U[i][0]):
+            i += 1
+        else: j -= 1
+
+def diameter(Points):
+    '''Given a list of 2d points, returns the pair that's farthest apart.'''
+    diam,pair = max([((p[0]-q[0])**2 + (p[1]-q[1])**2, (p,q))
+                     for p,q in rotatingCalipers(Points)])
+    return diam, pair
+
+def min_feret(Points):
+    '''Given a list of 2d points, returns the pair that's farthest apart.'''
+    min_feret_sq,pair = min([((p[0]-q[0])**2 + (p[1]-q[1])**2, (p,q))
+                     for p,q in rotatingCalipers(Points)])
+    return min_feret_sq, pair
+
+def list_shapes(viewer):
+    print(viewer.layers)
+    points = list(map(list,list(viewer.layers[1].data[0])))
+    pointarray = np.array(points)
+    chullpoints = pointarray[ConvexHull(pointarray).vertices, :]
+    print(chullpoints)
+    chull = list(map(list,list(chullpoints)))
+    uhull, lhull = hulls(points)
+    print("Upper hull", uhull)
+    print("lower hull", lhull)
+    print(viewer.layers[1].data)
+    _, c1, c2, c3, c4, _, _ = mep(chull)
+    mep_coords = np.array([c2,c1,c4,c3])
+    print(mep_coords)
+    layer = viewer.layers[1]
+    min_fdist, pair_min = min_feret(chull)
+    max_fdist, pair_max = diameter(chull)
+    layer.add([chullpoints, mep_coords], shape_type='polygon')
+    layer.add([np.array(pair_min), np.array(pair_max)], shape_type='line')
+    #viewer.add_shapes
+    #print(c1,c2,c3,c4)
+
+with napari.gui_qt():
+    viewer = napari.Viewer()
+    viewer.add_image(np.eye(256))
+    shape_layer = viewer.add_shapes([], name = "myshapes")
+    viewer.bind_key('9', list_shapes)
