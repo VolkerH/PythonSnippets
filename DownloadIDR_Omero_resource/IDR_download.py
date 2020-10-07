81ec0d663502be6c5aa77bffcec2ab9231e53e88
Volker <VolkerH>
2020-10-07 14:54:24 +1100

    added correct reshaping/dimension ordering and test

--- DownloadIDR_Omero_resource/IDR_download.py
+++ DownloadIDR_Omero_resource/IDR_download.py
@@ -1,3 +1,7 @@
+# Download raw pixel data from IDR
+# Volker.Hilsenstein@monash.edu
+# License BSD-3
+
 import numpy as np
 import omero
 from omero.gateway import BlitzGateway
@@ -37,7 +41,6 @@ def IDR_fetch_image(image_id: int, progressbar: bool = True) -> np.ndarray:
         _.getSizeX(),
         _.getSizeC(),
     )
-    img_shape = (nt, nz, ny, nx, nc)
 
     plane_indices = list(product(range(nz), range(nc), range(nt)))
     idr_plane_iterator = idr_pixels.getPlanes(plane_indices)
@@ -46,17 +49,29 @@ def IDR_fetch_image(image_id: int, progressbar: bool = True) -> np.ndarray:
         import tqdm
 
         idr_plane_iterator = tqdm.tqdm(idr_plane_iterator, total=len(plane_indices))
-    # TODO Need to work on proper reshaping
-    print("Warning. Reshaping not tested properly and likely wrong")
-    return np.asarray(list(idr_plane_iterator)).reshape(img_shape)
+
+    _tmp = np.asarray(list(idr_plane_iterator))
+    _tmp = _tmp.reshape((nz, nc, nt, ny, nx))
+    return np.einsum("jmikl", _tmp)
 
 
-def test_fetch():
-    """as a test download a sample image that we have previously downloaded
-    and see whether it matches previous hash and shape"""
+def test_fetch_2d_timelapse():
     sample_img_id = 1512575
     sample_img_shape = (93, 1, 1024, 1344, 1)
     sample_img_hash = "368793dbb3477c5acfce647cc05ebfc3d86e3b5757a39791111571dc4c305862"
+    import hashlib
+
+    img = IDR_fetch_image(sample_img_id)
+    h = hashlib.sha256(np.ascontiguousarray(img))
+    assert h.hexdigest() == sample_img_hash
+    assert img.shape == sample_img_shape
+
+
+# TODO: use parametrized testing to adhere to DRY principle
+def test_fetch_multichannel_zstack():
+    sample_img_id = 10502968
+    sample_img_shape = (1, 20, 480, 480, 3)
+    sample_img_hash = "44190d5ed14a2d138bdf901bbc66367cded8e7cb5fac0cc4dc4b064106749256"
     import hashlib
 
     img = IDR_fetch_image(sample_img_id)
